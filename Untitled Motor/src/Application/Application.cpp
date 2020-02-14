1833a4cac0c1a62c15aee90874f142e4c360a271
javiarias <javari02@ucm.es>
2020-02-14 10:58:57 +0100

    yikes 2

--- Untitled Motor/src/Application/Application.cpp
+++ Untitled Motor/src/Application/Application.cpp
@@ -7,10 +7,22 @@
 #include <OgreRoot.h>
 
 
-int main()
+
+#ifdef  _DEBUG
+    int main()
+#else
+    #include <Windows.h>
+    int WINAPI
+    WinMain(HINSTANCE hinstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow)
+#endif
 {
     Ogre::Root* root;
+
+#ifdef  _DEBUG
     root = new Ogre::Root("plugins_d.cfg");
+#else
+    root = new Ogre::Root("plugins.cfg");
+#endif
 
 
 
