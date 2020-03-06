c34a619a94227aaed7b351585d70c7a49ca3b0bb
javiarias <javari02@ucm.es>
2020-03-06 10:56:25 +0100

    SE HA INTENTADO

--- Untitled Motor/src/Application/Application.cpp
+++ Untitled Motor/src/Application/Application.cpp
@@ -3,10 +3,9 @@
 
 #include "Entity.h"
 
-#include <stdio.h>
+
 #include <OgreRoot.h>
 #include <SDL.h>
-#include <SDL_syswm.h>
 #include "windows.h"
 #include <iostream>
 #include <fmod.hpp>
@@ -23,11 +22,49 @@
 #include <OgreResourceManager.h>
 #include <OgreFileSystemLayer.h>
 #include <OgreGpuProgramManager.h>
+#include <OgreRTShaderSystem.h>
+
 
 using namespace FMOD;
 
 Ogre::Camera* cam;
 Ogre::Root* root;
+
+Ogre::FileSystemLayer* FSLayer;
+Ogre::RTShader::ShaderGenerator* ShaderGenerator; // The Shader generator instance.
+
+
+void SetupResources()
+{
+	using namespace Ogre;
+
+	// Load resource paths from config file
+	ConfigFile cf;
+	cf.load("resources.cfg");
+
+	// Go through all sections & settings in the file
+	ConfigFile::SectionIterator itSection = cf.getSectionIterator();
+
+	String strGroupName, strLocation, strType;
+	while (itSection.hasMoreElements())
+	{
+		// Resource group.  "General", etc
+		strGroupName = itSection.peekNextKey();
+
+		ConfigFile::SettingsMultiMap* mapSettings = itSection.getNext();
+		ConfigFile::SettingsMultiMap::iterator itSetting = mapSettings->begin();
+
+		for (; itSetting != mapSettings->end(); ++itSetting)
+		{
+			// Zip or FileSystem
+			strType = itSetting->first;
+			// Filepath
+			strLocation = itSetting->second;
+			ResourceGroupManager::getSingleton().addResourceLocation(strLocation, strType, strGroupName);
+		}
+	}
+	ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
+}
 
 #ifdef  _DEBUG
     int main(int argc, char* argv[])
@@ -38,29 +75,43 @@ Ogre::Root* root;
 #endif
 {
 	// Initialise OGRE
-		
+		Ogre::String pluginPath;
 #ifdef  _DEBUG
-    root = new Ogre::Root("plugins_d.cfg");
+		pluginPath = "plugins_d.cfg";
 #else
-    root = new Ogre::Root("plugins.cfg");
+		pluginPath = "plugins.cfg";
 #endif
 
+	FSLayer = new Ogre::FileSystemLayer("Motor Casa Paco");
+
+	root = new Ogre::Root(pluginPath, FSLayer->getWritablePath("ogre.cfg"), FSLayer->getWritablePath("ogre.log"));
+
+
 	Ogre::RenderSystem* rs = root->getRenderSystemByName("Direct3D11 Rendering Subsystem");
-	root->setRenderSystem(rs);
 	rs->setConfigOption("Full Screen", "No");
 	rs->setConfigOption("Video Mode", "800 x 600 @ 32-bit colour");
 	root->setRenderSystem(rs);
 
+
 	Ogre::String mSolutionPath;
-	mSolutionPath = "C:\\Users\\Briso\\OneDrive\\Documentos\\GitHub\\Untitled-Motor\\Untitled Motor\\bin";
+	mSolutionPath = "./assets";
 
 	Ogre::ResourceGroupManager::getSingleton().addResourceLocation(mSolutionPath, "FileSystem", "General");
 	
 	Ogre::RenderWindow* mWindow = root->initialise(true, "Motor Casa Paco");
 
+	SetupResources();
+
 	Ogre::TextureManager::getSingleton().setDefaultNumMipmaps(5);
 
-	Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();  
+
+	if (Ogre::RTShader::ShaderGenerator::initialize())
+	{
+		ShaderGenerator = Ogre::RTShader::ShaderGenerator::getSingletonPtr();
+		// Core shader libs not found -> shader generating will fail.
+		// Create and register the material manager listener if it doesn't exist yet.
+	}
+
 
 	Ogre::SceneManager* mSM = root->createSceneManager();
 	Ogre::Camera* mCamera=mSM->createCamera("MainCam");
@@ -78,120 +129,14 @@ Ogre::Root* root;
 
 	Ogre::Entity* ogreEntity = mSM->createEntity("sphere.mesh");
 	
+	
 	Ogre::SceneNode* Node = mSM->getRootSceneNode()->createChildSceneNode();
-	Node->attachObject(ogreEntity);//PETA AQUI
-	//Node->setPosition(400, 100, -300);
-	//Node->setScale(20, 20, 20);
-
-	/*
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-		OGRE_EXCEPT(Ogre::Exception::ERR_INTERNAL_ERROR, "Cannot initialize SDL2!",
-					"BaseApplication::setup");
-		SDL_Quit();
-		return 1;
-	}
-
-	root->setRenderSystem(*(root->getAvailableRenderers().begin()));
-
-	root->restoreConfig();
-	root->initialise(false);
-
-	Ogre::NameValuePairList params; // ogre window / render system params
-	*/
-	//Ogre::RenderWindow* ogreWindow = root->createRenderWindow("myWindowTitle", 800, 600, false, &params);
-
-	// create OGRE scene manager, camera, viewports, etc
-
-	// FMOD TESTING
-	FMOD::System* system;
-	FMOD_RESULT result;
-	result = FMOD::System_Create(&system); // Creamos el objeto system
-	// 128 canales (numero maximo que podremos utilizar simultaneamente)
-	result = system->init(128, FMOD_INIT_NORMAL, 0); // Inicializacion de FMOD
-
-	FMOD::Sound* sound;
-	result = system->createSound(
-		"assets/sound/startup.mp3", // path al archivo de sonido
-		FMOD_DEFAULT, // valores (por defecto en este caso: sin loop, 2D)
-		0, // informacion adicional (nada en este caso)
-		&sound);
-
-	FMOD::Channel* channel;
-	result = system->playSound(
-		sound, // buffer que se "engancha" a ese canal
-		0, // grupo de canales, 0 sin agrupar (agrupado en el master)
-		false, // arranca sin "pause" (se reproduce directamente)
-		&channel); // devuelve el canal que asigna
-		// el sonido ya se esta reproduciendo
-
-		///-----initialization_start-----
-
-	// BULLET PHYSICS TESTING
-	///collision configuration contains default setup for memory, collision setup. Advanced users can create their own configuration.
-	btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
-
-	///use the default collision dispatcher. For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
-	btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
-
-	///btDbvtBroadphase is a good general purpose broadphase. You can also try out btAxis3Sweep.
-	btBroadphaseInterface* overlappingPairCache = new btDbvtBroadphase();
-
-	///the default constraint solver. For parallel processing you can use a different solver (see Extras/BulletMultiThreaded)
-	btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
-
-	btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
-
-	dynamicsWorld->setGravity(btVector3(0, -10, 0));
-
-	///-----initialization_end-----
-
-	//keep track of the shapes, we release memory at exit.
-	//make sure to re-use collision shapes among rigid bodies whenever possible!
-	btAlignedObjectArray<btCollisionShape*> collisionShapes;
-	/*
-	SDL_Window* sdlWindow = SDL_CreateWindow("myWindow", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
-	if (sdlWindow == nullptr) {
-		OGRE_EXCEPT(Ogre::Exception::ERR_INTERNAL_ERROR,
-			"Couldn't create SDL Window" + *SDL_GetError(),
-			"BaseApplication::setup");
-		SDL_Quit();
-		return 1;
-	}
-
-	SDL_Renderer* renderer = SDL_CreateRenderer(sdlWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (renderer == nullptr) {
-		OGRE_EXCEPT(Ogre::Exception::ERR_INTERNAL_ERROR,
-			"SDL_CreateRenderer Error: " + *SDL_GetError(),
-			"BaseApplication::setup");
-		SDL_DestroyWindow(sdlWindow);
-		SDL_Quit();
-		return 1;
-	}
-
-	SDL_SysWMinfo wmInfo;
-	SDL_VERSION(&wmInfo.version);
-	if (SDL_GetWindowWMInfo(sdlWindow, &wmInfo) == SDL_FALSE) {
-		OGRE_EXCEPT(Ogre::Exception::ERR_INTERNAL_ERROR,
-			"Couldn't get WM Info! (SDL2)",
-			"BaseApplication::setup");
-		SDL_DestroyWindow(sdlWindow);
-		SDL_Quit();
-		return 1;
-	}
-
-	// grab a string representing the NSWindow pointer
-	Ogre::String winHandle = Ogre::StringConverter::toString((unsigned long)wmInfo.info.win.window);
-
-	// assign the NSWindow pointer to the parentWindowHandle parameter
-	params.insert(std::make_pair("externalWindowHandle", winHandle));
-	*/
-	//COSAS
+	Node->attachObject(ogreEntity);
 
 
-	//cam->setPosition(0, 0, 80);
-	//cam->lookAt(0, 0, -300);
-	//cam->setNearClipDistance(5);
-	//
+
+
+	//root->startRendering();
 
 	while (true)
 	{
@@ -206,8 +151,6 @@ Ogre::Root* root;
 
 	// clean up
 	delete root;
-	//SDL_DestroyRenderer(renderer);
-	//SDL_DestroyWindow(sdlWindow);
 	SDL_Quit();
 
     return 0;
