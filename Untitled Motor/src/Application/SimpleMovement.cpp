db699f00eb21d324435589c2f4d1e73388383225
PabloGarciaGrossi <pabgar20@ucm.es>
2020-03-31 20:37:26 +0200

    me dice que si no me borra cosas

--- Untitled Motor/src/Application/SimpleMovement.cpp
+++ Untitled Motor/src/Application/SimpleMovement.cpp
@@ -1,6 +1,7 @@
 #include "SimpleMovement.h"
 #include "InputManager.h"
 #include "Transform.h"
+#include "RigidBody.h"
 #include "Entity.h"
 #include "Core.h"
 SimpleMovement::SimpleMovement(json& args) : Component(args)
@@ -8,7 +9,10 @@ SimpleMovement::SimpleMovement(json& args) : Component(args)
 	speed = args["speed"];
 	rotateSpeed = args["rotateSpeed"];
 }
+SimpleMovement::~SimpleMovement()
+{
 
+}
 
 void SimpleMovement::start()
 {
@@ -20,18 +24,24 @@ void SimpleMovement::update()
 	if (InputManager::getInstance()->IsKeyDown(SDL_SCANCODE_RIGHT))
 	{
 		Transform* transform = getEntity()->getComponent<Transform>("Transform");
-		transform->setRotation(transform->getRotation + (rotateSpeed * Core::getInstance()->DeltaTime() / 1000.0f));
+		transform->rotate(Ogre::Vector3(0, rotateSpeed * Core::getInstance()->DeltaTime(), 0));
 	}
 
 	if (InputManager::getInstance()->IsKeyDown(SDL_SCANCODE_LEFT))
 	{
 		Transform* transform = getEntity()->getComponent<Transform>("Transform");
-		transform->setRotation(transform->getRotation - (rotateSpeed * Core::getInstance()->DeltaTime() / 1000.0f));
+		transform->rotate(Ogre::Vector3(0, - rotateSpeed * Core::getInstance()->DeltaTime(), 0));
 	}
 
 	if (InputManager::getInstance()->IsKeyDown(SDL_SCANCODE_DOWN))
 	{
 		Transform* transform = getEntity()->getComponent<Transform>("Transform");
-		//transform->setPosition(transform->getPosition - (spee))
+		transform->translate(Ogre::Vector3(0, 0, - speed * Core::getInstance()->DeltaTime()), Ogre::Node::TS_LOCAL);
+	}
+
+	if (InputManager::getInstance()->IsKeyDown(SDL_SCANCODE_UP))
+	{
+		Transform* transform = getEntity()->getComponent<Transform>("Transform");
+		transform->translate(Ogre::Vector3(0, 0, speed * Core::getInstance()->DeltaTime()), Ogre::Node::TS_LOCAL);
 	}
 }
