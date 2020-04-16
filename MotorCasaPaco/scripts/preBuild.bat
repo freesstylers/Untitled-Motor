26091f5c1aa931418ec3c200147c5cc2d9f4cfb5
Sergio Abreu García <saggamedeveloper@gmail.com>
2020-04-16 19:13:46 +0200

    Rutas relativas al proyecto y script para los juegos

--- MotorCasaPaco/scripts/preBuild.bat
+++ MotorCasaPaco/scripts/preBuild.bat
@@ -1,7 +1,13 @@
-for /r ..\dependencies\ogre-1.12.4\Build\bin\debug\ %%f in (*.dll) do @copy "%%f" ..\bin\
-for /r ..\dependencies\ogre-1.12.4\Build\bin\release\ %%f in (*.dll) do @copy "%%f" ..\bin\
-for /r ..\dependencies\SDL2-2.0.10\lib\x64 %%f in (*.dll) do @copy "%%f" ..\bin\
-for /r ..\dependencies\fmod-2.00.07\ %%f in (*.dll) do @copy "%%f" ..\bin\
-for /r ..\dependencies\bullet3-2.89\build\lib %%f in (*.dll) do @copy "%%f" ..\bin\
-for /r ..\dependencies\cegui-0.8.7\build\bin\ %%f in (*.dll) do @copy "%%f" ..\bin\
-for /r ..\dependencies\cegui-0.8.7\dependencies\bin %%f in (*.dll) do @copy "%%f" ..\bin\
+SET ogreMediaTarget="%~1\assets\ogre\Media"
+SET ogreTestsMediaTarget="%~1\assets\ogre\Tests\Media"
+
+xcopy ..\dependencies\ogre-1.12.4\Media %ogreMediaTarget% /s /e /y /i
+xcopy ..\dependencies\ogre-1.12.4\Tests\Media %ogreTestsMediaTarget% /s /e /y /i
+
+for /r ..\dependencies\ogre-1.12.4\Build\bin\debug\ %%f in (*.dll) do @copy "%%f" %1%
+for /r ..\dependencies\ogre-1.12.4\Build\bin\release\ %%f in (*.dll) do @copy "%%f" %1%
+for /r ..\dependencies\SDL2-2.0.10\lib\x64 %%f in (*.dll) do @copy "%%f" %1%
+for /r ..\dependencies\fmod-2.00.07\ %%f in (*.dll) do @copy "%%f" %1%
+for /r ..\dependencies\bullet3-2.89\build\lib %%f in (*.dll) do @copy "%%f" %1%
+for /r ..\dependencies\cegui-0.8.7\build\bin\ %%f in (*.dll) do @copy "%%f" %1%
+for /r ..\dependencies\cegui-0.8.7\dependencies\bin %%f in (*.dll) do @copy "%%f" %1%
